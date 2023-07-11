3d47ded659ed93058d0baece46ae1cef0805ffa8
House <118424356+house40105@users.noreply.github.com>
2023-07-11 11:05:52 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 創建logs目錄
+mkdir -p /home/logs/TableHousekeeping/
+
+# 創建程式目錄
+mkdir -p /home/TableHousekeeping/
+
+# 複製到指定目錄
+cp /tmp/TableHousekeeping/* /home/TableHousekeeping/
+chmod 754 /home/TableHousekeeping/TableHousekeeping.pl
+
+# 自動設定到Crontab (判斷是否crontab已有)
+if crontab -l | grep -q 'TableHousekeeping'; then
+    echo "Crontab entry already exists. Skipping..."
+else
+    # 添加Crontab任務，這裡的範例是每天凌晨1點執行腳本
+    echo "0 1 * * * /home/TableHousekeeping/TableHousekeeping.pl" | crontab -
+fi
