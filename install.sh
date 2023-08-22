51418e22790998219521afd65fc852f34a69b37b
House <118424356+house40105@users.noreply.github.com>
2023-08-22 10:44:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,24 @@ mkdir -p /home/logs/TableHousekeeping/
 mkdir -p /home/TableHousekeeping/
 
 # 複製到指定目錄
-cp /tmp/TableHousekeeping/TableHousekeeping-main /home/TableHousekeeping/
+cp /tmp/TableHousekeeping/TableHousekeeping /home/TableHousekeeping/
 chmod 754 /home/TableHousekeeping/TableHousekeeping.pl
 
+
+LIST=`crontab -l`
+MEMO="### The Script is tableHousekeeping and syslog trogger"
+SOURCE="/usr/bin/perl /home/TableHousekeeping/TableHousekeeping.pl > /dev/null"
+
 # 自動設定到Crontab (判斷是否crontab已有)
-if crontab -l | grep -q 'TableHousekeeping'; then
+if echo "$LIST" | grep -q "$SOURCE"; then
     echo "Crontab entry already exists. Skipping..."
 else
     # 添加Crontab任務，這裡的範例是每天凌晨1點執行腳本
-    echo "0 1 * * * /home/TableHousekeeping/TableHousekeeping.pl" | crontab -
+    crontab -l | { cat; echo "$MEMO"; } | crontab -
+    crontab -l | { cat; echo "0 1 * * * $SOURCE"; } | crontab -
 fi
+
+   echo "## Crontab list ..."
+   echo "======================================================="
+   crontab -l
+   echo "======================================================="
